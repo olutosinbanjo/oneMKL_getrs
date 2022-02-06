8546096c9b83d4c394f3790cbb469c0fa506dfbd
Oluwatosin Odubanjo <olutosinbanjo@gmail.com>
2022-02-06 22:58:14 +0100

    Add files via upload

---
+++ job/getrs_usm_het2.sh
@@ -0,0 +1,34 @@
+#!bin/bash -f
+
+#PBS -l nodes=1:gpu:ppn=2,walltime=12:00:00
+#PBS -N getrs_usm_het2
+cd ~/GITHUB/GETRS/bin/
+
+echo SIZE=500
+./getrs_usm_het2 500
+./getrs_usm_het2 500
+./getrs_usm_het2 500
+./getrs_usm_het2 500
+./getrs_usm_het2 500
+
+echo SIZE=2500
+./getrs_usm_het2 2500
+./getrs_usm_het2 2500
+./getrs_usm_het2 2500
+./getrs_usm_het2 2500
+./getrs_usm_het2 2500
+
+echo SIZE=5000
+./getrs_usm_het2 5000
+./getrs_usm_het2 5000
+./getrs_usm_het2 5000
+./getrs_usm_het2 5000
+./getrs_usm_het2 5000
+
+echo SIZE=10000
+./getrs_usm_het2 10000
+./getrs_usm_het2 10000
+./getrs_usm_het2 10000
+./getrs_usm_het2 10000
+./getrs_usm_het2 10000
+
