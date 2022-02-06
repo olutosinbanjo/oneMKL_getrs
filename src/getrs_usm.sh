4d21155c82ecdcd15d25ba6d092a7bcec6003ebe
Oluwatosin Odubanjo <olutosinbanjo@gmail.com>
2022-02-06 22:57:24 +0100

    Add files via upload

---
+++ src/getrs_usm.sh
@@ -0,0 +1,34 @@
+#!bin/bash -f
+
+#PBS -l nodes=1:gpu:ppn=2,walltime=12:00:00
+#PBS -N getrs_usm
+cd ~/GITHUB/GETRS/bin/
+
+echo SIZE=500
+./getrs_usm 500
+./getrs_usm 500
+./getrs_usm 500
+./getrs_usm 500
+./getrs_usm 500
+
+echo SIZE=2500
+./getrs_usm 2500
+./getrs_usm 2500
+./getrs_usm 2500
+./getrs_usm 2500
+./getrs_usm 2500
+
+echo SIZE=5000
+./getrs_usm 5000
+./getrs_usm 5000
+./getrs_usm 5000
+./getrs_usm 5000
+./getrs_usm 5000
+
+echo SIZE=10000
+./getrs_usm 10000
+./getrs_usm 10000
+./getrs_usm 10000
+./getrs_usm 10000
+./getrs_usm 10000
+
